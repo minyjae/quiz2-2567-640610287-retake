0811114b0a8b65d3cb583bdb650b4e8620f20067
minyjae <jiradate_ora@cmu.ac.th>
2024-09-30 16:33:36 +0700

    pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import Comment from "@/components/Comment";
+import Reply from "@/components/Reply";
+import PostOwnner from "@/components/PostOwnner";
+import { comments } from "@/libs/comments";
 
 export default function Home() {
   return (
@@ -11,83 +15,22 @@ export default function Home() {
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
-        {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
-
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+          <PostOwnner name="จิรเดช อรทัย" id= "640610287"/>
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
-
+          {/* <Comment userImagePath="/profileImages/lisa.jpg" username="Lisa" commentText="จริงค่า" likeNum= {999} replies={comments[0].replies}/> */}
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
+          <Reply userImagePath="/profileImages/puppy.jpg" username="หมาน้อย" replyText="จริงค้าบบบบบบบ" likeNum= {0}/>
+          
+          {comments.map((comment) => (
+          <Comment 
+            userImagePath={comment.userImagePath}
+            username={comment.username}
+            commentText={comment.commentText}
+            likeNum={comment.likeNum}
+            replies={comment.replies}
           />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* map-loop render Comment component here */}
+        ))}
       </div>
     </div>
   );
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,25 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
 
-interface ReplyProps {}
+interface CommentProps {
+    userImagePath : string;
+    username : string;
+    commentText : string;
+    likeNum : number;
+    replies : ReplyProps[];
+}
+export type { CommentProps };
+
+interface PostOwnerProps {
+    name : string;
+    id : string;
+}
+export type { PostOwnerProps };
+
+interface ReplyProps {
+    userImagePath : string;
+    username : string;
+    replyText : string;
+    likeNum : number;
+}
 export type { ReplyProps };
